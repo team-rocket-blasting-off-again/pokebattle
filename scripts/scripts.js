e9201ccde7521101405ece89f0dbfbd2f14d391e
Audra T <audraturuta@gmail.com>
2020-02-14 16:16:47 -0500

    js-starter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,14 @@
-console.log('test');
+const pokeBattleApp = {};
+
+pokeBattleApp.apiKeyAudra = 'Sm2MqrmUv3DBesJ7wy4vRTdhzYEsywi3',
+
+
+pokeBattleApp.init = function () {
+    // Start App
+    // Change user choice on click
+    // Battle Button
+};
+
+$(function () {
+    pokeBattleApp.init();
+});
