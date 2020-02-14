e5e63fef00a9b590b9fb99caa0d261518bc069c6
Matthew Hernandez <matt.hernandez94@gmail.com>
2020-02-14 17:02:33 -0500

    created base functionality

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,47 @@
 
-// namespace
-const pokebattle = {};
+// Namespace
+const pokebattle = {
+
+    rock: "./",
+    grass: "",
+    flying: "",
+    browser: [rock, grass, flying],
+};
+
+
 
 pokebattle.apiKeyAudra = 'Sm2MqrmUv3DBesJ7wy4vRTdhzYEsywi3',
 
+pokebattle.userSelection = function() {
+    
+    $("button").on("click", function() {
+        const selected = this.className;
+        console.log(selected);
+        // append html img with string in object
+    });
+};
+
+pokebattle.browserSelection = function() {
+    
+};
+
+
+ 
+// Battle function
+pokebattle.battle = function() {
+    // pokemon selection event
+};
+
 // Init function
 pokebattle.init = function() {
+    this.battle();
+    this.userSelection();
 };
 
 // Doc Ready function
 $(function () {
     pokebattle.init();
 });
+
+
+
